8a3fa5adbe9e0ef7b3b18625008fe11a66cc5e09
Cata001 <catalinateguiani@yahoo.com>
2021-08-25 17:50:56 +0100

    Work on 3 stopwatches

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let buttonStop = document.getElementById("stop");
 let buttonReset = document.getElementById("reset");
 let interval;
 
-function startTimer() {
+function startTimer(i) {
   milliseconds++;
 
   if (milliseconds < 10) {
@@ -59,4 +59,75 @@ buttonReset.onclick = function () {
   appendMinutes.innerHTML = minutes;
   appendSeconds.innerHTML = seconds;
   appendMilliseconds.innerHTML = milliseconds;
+};
+// let i = 0;
+// setInterval(function () {
+//   let nextPost = document.querySelector(".coreSpriteRightPaginationArrow");
+//   nextPost.click();
+// i++;
+//   console.log(`Browsed ${i} Posts`);
+//   console.log(i);
+// }, 4000);
+
+let seconds1 = 0;
+let minutes1 = 0;
+let milliseconds1 = 0;
+let appendMinutes1 = document.getElementById("minutes1");
+let appendSeconds1 = document.getElementById("seconds1");
+let appendMilliseconds1 = document.getElementById("milliseconds1");
+let buttonStart1 = document.getElementById("start1");
+let buttonStop1 = document.getElementById("stop1");
+let buttonReset1 = document.getElementById("reset1");
+let interval1;
+
+function startTimer1() {
+  milliseconds1++;
+
+  if (milliseconds1 < 10) {
+    appendMilliseconds1.innerHTML = "0" + milliseconds1;
+  }
+
+  if (milliseconds1 > 9) {
+    appendMilliseconds1.innerHTML = milliseconds1;
+  }
+
+  if (milliseconds1 > 59) {
+    seconds1++;
+    appendSeconds1.innerHTML = "0" + seconds1;
+    milliseconds1 = 0;
+    appendMilliseconds1.innerHTML = "0" + 0;
+  }
+
+  if (seconds1 > 9) {
+    appendSeconds1.innerHTML = seconds1;
+  }
+
+  if (seconds1 > 59) {
+    minutes1++;
+    appendMinutes1.innerHTML = "0" + minutes1;
+    seconds1 = 0;
+    appendSeconds1.innerHTML = "0" + 0;
+  }
+
+  if (minutes1 > 9) {
+    appendMinutes1.innerHTML = minutes1;
+  }
+}
+
+buttonStart1.onclick = function () {
+  interval1 = setInterval(startTimer1, 1000);
+};
+
+buttonStop1.onclick = function () {
+  clearInterval(interval1);
+};
+
+buttonReset1.onclick = function () {
+  clearInterval(interval1);
+  milliseconds1 = "00";
+  seconds1 = "00";
+  minutes1 = "00";
+  appendMinutes1.innerHTML = minutes1;
+  appendSeconds1.innerHTML = seconds1;
+  appendMilliseconds1.innerHTML = milliseconds1;
 };
